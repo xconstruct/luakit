555b13f22fa35af286b3c6c81b34d8ee3f54319d
Roman Leonov <rliaonau@gmail.com>
2012-02-11 05:47:56 +0300

    [vh]box_new() -> box_new()

--- widgets/box.c
+++ widgets/box.c
@@ -128,9 +128,7 @@ widget_box(widget_t *w, luakit_token_t token)
     w->newindex = luaH_box_newindex;
     w->destructor = widget_destructor;
 
-    w->widget = gtk_box_new(FALSE, 0);
-    gtk_orientable_set_orientation(GTK_ORIENTABLE(w->widget),
-            (token == L_TK_VBOX) ? GTK_ORIENTATION_VERTICAL : GTK_ORIENTATION_HORIZONTAL);
+    w->widget = gtk_box_new((token == L_TK_VBOX) ? GTK_ORIENTATION_VERTICAL : GTK_ORIENTATION_HORIZONTAL, 0);
 
     g_object_set_data(G_OBJECT(w->widget), "lua_widget", (gpointer) w);
     g_object_connect(G_OBJECT(w->widget),
